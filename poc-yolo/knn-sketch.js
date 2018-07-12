08e1ef89e067eaa841f3118e5577f9108706c811
dex1t <dex1t@deg.ooo>
2018-07-12 18:09:10 +0200

    WIP

--- poc-yolo/knn-sketch.js
+++ poc-yolo/knn-sketch.js
@@ -1,14 +1,8 @@
-// ------- yolo ------
-let video;
-let yolo;
-let status;
-let objects = [];
-let count;
-let player;
+//Basic KNN classification of MFFCs
 
-// ------- knn ------
 var k = 3; //k can be any integer
 var machine = new kNear(k);
+var test;
 var currentClass = 0;
 var nSamples = 0;
 
@@ -18,7 +12,9 @@ var normalized = [];
 var mfcc;
 var loudness = 0;
 var loudnessThreshold = 10;
+
 var soundReady = false;
+
 
 //TRIGGER MODE
 var predictionAlpha = 255;
@@ -30,41 +26,12 @@ var timer;
 var test = 0;
 
 function setup() {
-  createCanvas(640, 480);
-  video = createCapture(VIDEO);
-  video.size(640, 480);
-
-  // Create a YOLO method
-  yolo = ml5.YOLO(video, () => {
-    status.html('Model Loaded👌');
-    detect();
-  });
-
-  // Hide the original video
-  video.hide();
-  status = select('#status');
-  videoStatus = select('#video-status');
-
-  setupYoutube();
-
-  audio = new MicrophoneInput(512);
-  startTime = millis();
+    createCanvas(640, 480);
+    audio = new MicrophoneInput(512);
+    startTime = millis();
 }
 
 function draw() {
-  image(video, 0, 0, width, height);
-  for (let i = 0; i < objects.length; i++) {
-    noStroke();
-    fill(0, 255, 0);
-    text(objects[i].className, objects[i].x * width, objects[i].y * height - 5);
-    noFill();
-    strokeWeight(4);
-    stroke(0, 255, 0);
-    rect(objects[i].x * width, objects[i].y * height, objects[i].w * width, objects[i].h * height);
-  }
-}
-
-function drawKNN() {
     background(255);
     textSize(12);
 
@@ -137,6 +104,8 @@ function drawKNN() {
     text(" nSamples: " + nSamples, width -350, 35);
 
     if (predictionAlpha > 0) predictionAlpha-=5;
+
+
 }
 
 function soundDataCallback(soundData) {
@@ -175,73 +144,3 @@ function keyPressed() {
         currentClass = 9;
     }
 }
-
-let videoFlag = 'blank';
-function detect() {
-  yolo.detect(function(results) {
-    objects = results;
-
-    count = _.filter(objects, { 'className': 'person' }).length;
-    status.html(`Number of person: ${count}`);
-
-    if (count > 0) {
-      if (videoFlag != 'movie') {
-        playMovieVideo();
-        // player.playVideo();
-        videoFlag = 'movie'
-      }
-      videoStatus.html('Playing video ▶️')
-    } else {
-      if (videoFlag != 'blank') {
-        playBlankVideo();
-        // player.pauseVideo();
-        videoFlag = 'blank'
-      }
-      videoStatus.html('Pausing video ⏸')
-    }
-    detect();
-  });
-}
-
-// --------- youtube -----------
-
-function setupYoutube() {
-  let tag = document.createElement('script');
-  tag.src = "https://www.youtube.com/iframe_api";
-  let firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-}
-
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('youtube', {
-    height: '360',
-    width: '640',
-    videoId: '',
-    videoId: '8tPnX7OPo0Q',
-    playerVars: { 'autoplay': 1, 'rel': 0, 'showinfo': 0 },
-    events: {
-      'onReady': onPlayerReady,
-      // 'onStateChange': onPlayerStateChange
-    }
-  });
-}
-
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
-
-function playBlankVideo() {
-  player.loadVideoById({'videoId': '8tPnX7OPo0Q'});
-}
-
-function playMovieVideo() {
-  player.loadVideoById({'videoId': 'g_Czx6qdKJo'});
-}
-
-// var done = false;
-// function onPlayerStateChange(event) {
-//   if (event.data == YT.PlayerState.PLAYING && !done) {
-//     // setTimeout(stopVideo, 6000);
-//     done = true;
-//   }
-// }
